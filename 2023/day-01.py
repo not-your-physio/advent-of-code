e2880e3c02c4063b71693f77edfc2dc92e4406cc
Will Harding <152809386+not-your-physio@users.noreply.github.com>
2024-02-26 19:52:39 +0000

    Add day 1 2023

---
+++ 2023/day-01.py
@@ -0,0 +1,29 @@
+adventofcode1docpath = "day-01.txt"
+print(adventofcode1docpath)
+
+with open(adventofcode1docpath, 'r') as file:
+    originalinput = ''.join(file.readlines()).strip()
+
+chunks = originalinput.split(" ")
+print(chunks)
+
+def getchunknumbers(chunk):
+    numbers = []
+    for character in chunk:
+        if character.isdigit():
+            numbers.append(int(character))
+    return numbers
+    
+
+def sumchunknumbers(numbers):
+    return numbers[0]*10 + numbers[-1]
+
+total = 0
+for chunk in chunks: total += (sumchunknumbers(getchunknumbers(chunk)))
+print(total)
+
+
+
+
+
+
